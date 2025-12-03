18ca2039b67f47de43ad0b10d6fb45d09167c793
daniel0406070 <daniel0406070@gmail.com>
2025-12-03 11:27:07 +0900

    refactor: Clean up layers module
    - Remove unused layer implementations (Embed, SelfAttention, Transformer)
    - Remove Vector_Context_Align and ranger21 optimizer
    - Simplify __init__.py imports

--- layers/__init__.py
+++ layers/__init__.py
@@ -1,1 +1,4 @@
-
+# TS-CVA Layers
+from .Cross_Modal_Align import CrossModal, TSTEncoderLayer
+from .StandardNorm import Normalize
+from .TS_Pos_Enc import positional_encoding, Transpose, get_activation_fn
