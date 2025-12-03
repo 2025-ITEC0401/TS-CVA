0f2c23643fc3d781637147ffd1735f00dffa7b9b
daniel0406070 <daniel0406070@gmail.com>
2025-12-03 11:27:25 +0900

    refactor: Remove old shell scripts
    - Remove baseline experiment scripts (ECL, ETT, FRED, etc.)
    - Remove pretrain and storage scripts
    - Remove tscva training scripts
    - Clean up scripts directory

--- scripts/utils/find_free_gpu.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Find free GPU with lowest memory usage
-# Usage: CUDA_ID=$(bash find_free_gpu.sh)
-
-nvidia-smi --query-gpu=index,memory.used --format=csv,noheader,nounits | \
-  awk -F', ' '{print $2, $1}' | \
-  sort -n | \
-  head -1 | \
-  awk '{print $2}'
